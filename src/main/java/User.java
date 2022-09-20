66cb8e4acb80a93b4059aad2d4524325034e57fe
Ben <benprice1998@gmail.com>
2022-09-20 13:22:51 -0400

    Backend

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,6 +5,16 @@ public class User {
     Date dob;
     int userID;
 
+    public User(){
 
+    }
+    public User(String fn, String ln, String address, String email, Date dob,int userID){
+        this.fname = fn;
+        this.lname = ln;
+        this.address = address;
+        this.email = email ;
+        this.dob = dob;
+        this.userID = userID;
 
+    }
 }
