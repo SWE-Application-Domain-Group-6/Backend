43704eb4b25bb14c526d5f0d25587a589fd24183
andersonguse <andersonguse53469@gmail.com>
2022-10-30 17:34:45 -0400

    changed servlet imports

--- src/main/java/updateuser.java
+++ src/main/java/updateuser.java
@@ -1,6 +1,9 @@
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.sql.SQLException;
