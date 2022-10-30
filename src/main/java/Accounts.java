4830bba64e027f45d2e2e7c681461e39f25aa68f
andersonguse <andersonguse53469@gmail.com>
2022-10-30 17:36:50 -0400

    more servlet imports

--- src/main/java/Accounts.java
+++ src/main/java/Accounts.java
@@ -1,6 +1,9 @@
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 
 import java.io.IOException;
 import java.sql.Connection;
